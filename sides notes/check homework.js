c6378261cf4e1a8a8a1dba1ee7e527ce741f9236
Raina86 <you@example.com>
2020-10-06 15:20:20 -0400

    Extra modifications

---
+++ sides notes/check homework.js
@@ -0,0 +1,33 @@
+let myMovie = {
+    nameOfMovie: "The Social Network",
+    runTime: "228",
+    genre: "Drama",
+
+    characters: [{
+            charOne: {
+                name: "Legolas",
+                age: "2931",
+                items: [{ eyes: "blue", hair: "black" }],
+            }
+
+        },
+        {
+            charTwo: {
+                name: "Gandalf",
+                age: "2019",
+                items: [{ eyes: "brown", hair: "blonde" }]
+            }
+
+        },
+    ]
+
+}
+
+console.log(myMovie.nameOfMovie)
+console.log(myMovie.runTime)
+    //console.log(myMovie.characters)
+
+
+Function SayHi(luke’, ‘skywalker’)
+
+console.log(names)
